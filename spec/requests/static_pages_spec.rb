40550c2047b5785506fd40267b3ce5a5c021fd5c
David McMullin <david.mcmullin@gmail.com>
2012-10-07 21:38:40 +0100

    add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -55,6 +55,17 @@ describe "StaticPages" do
         visit root_path
         page.should have_content("1 micropost")
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
   end
 
